43437aa40f1c5a43f205ac8fa93bd52b7418450e
Paper <fredchowdz@gmail.com>
2020-09-30 08:19:19 +0800

    Initial Commit

---
+++ Intimacy-Semantics/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  Intimacy-Semantics
+//
+//  Created by Zhou Wei Ran on 2020/9/30.
+//  Copyright © 2020 Paper Scratch. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ Intimacy-Semantics/DocumentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DocumentViewController.swift
+//  Intimacy-Semantics
+//
+//  Created by Zhou Wei Ran on 2020/9/30.
+//  Copyright © 2020 Paper Scratch. All rights reserved.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
